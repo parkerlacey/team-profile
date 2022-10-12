5b44e49149b2bfd0eb38aa522c5998b73a441042
Parker Lacey <parkerlacey2424@gmail.com>
2022-10-11 18:38:14 -0600

    updated template

---
+++ index.js
@@ -0,0 +1,6 @@
+const Manager = require('./lib/manager.js');
+const Engineer = require('./lib/engineer.js');
+const Intern = require('./lib/intern.js');
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
