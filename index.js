117b00b1a010f33907c78b8b8ef61be44c657c2d
Parker Lacey <parkerlacey2424@gmail.com>
2022-10-11 18:59:45 -0600

    updated index.js file

--- index.js
+++ index.js
@@ -1,6 +1,78 @@
+const template = require('./src/template.js')
+
+//team profiles
 const Manager = require('./lib/manager.js');
 const Engineer = require('./lib/engineer.js');
 const Intern = require('./lib/intern.js');
+
+// node modules
 const inquirer = require("inquirer");
-const path = require("path");
 const fs = require("fs");
+
+
+//team array
+const teamArray = [];
+
+const addManager = () => {
+  return inquirer.prompt ([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'Who is the manager of this team?',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter the manager's name!");
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: "Please enter the manager's ID.",
+      validate: nameInput => {
+        if (isNaN(nameInput)) {
+          console.log ("please enter the manager's ID!")
+          return false;
+        } else {
+          return true;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: "Please enter the manager's email.",
+      validate: email => {
+        valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+        if (valid) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'officeNumber',
+      message: "Please enter the manager's office number",
+      validate: nameInput => {
+        if (isNaN(nameInput)) {
+          console.log('Please enter an office number!')
+          return false;
+        } else {
+          return true;
+        }
+      }
+    }
+  ])
+  .then(managerInput => {
+    const { name, id, email, officeNumber } = managerInput;
+    const manager = new Manager (name, id, email, officeNumber);
+    teamArray.push(manager);
+    console.log(manager);
+  })
+};
+
